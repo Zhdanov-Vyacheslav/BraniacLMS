1b7a6f1359d6adb83faa145269198fb65290b966
Zhdanov-Vyacheslav <90984177+Zhdanov-Vyacheslav@users.noreply.github.com>
2022-05-28 21:16:44 +0300

    Lesson 3 (#2)
    lesson_3 Lesson rdy

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,6 @@
+import json
+from datetime import datetime
+
 from django.views.generic import TemplateView
 
 
@@ -7,6 +10,33 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        with open("test.json") as f:
+            data = json.loads(f.read())
+        context["news_list"] = data["news"]
+        context["datetime_obj"] = datetime.now()
+        return context
+
+    # def get_context_data(self, **kwargs):
+    #     # Get all previous data
+    #     context = super().get_context_data(**kwargs)
+    #     # Create your own data
+    #     context["news_title"] = "Громкий новостной заголовок"
+    #     context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+    #     context["range"] = range(5)
+    #     context["datetime_obj"] = datetime.now()
+    #     return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
 
 
 class CoursesPageView(TemplateView):
